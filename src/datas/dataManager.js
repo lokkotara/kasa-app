2b63a952fbe94d6c6001ff8d01b396fe8412dbd0
greg <lokkotarakun@gmail.com>
2022-04-15 13:19:13 +0200

    feat: suite de la section à propos

--- src/datas/dataManager.js
+++ src/datas/dataManager.js
@@ -1,5 +1,8 @@
 import accomodations from './logements.json'
+import aboutElements from './about.json'
 const Accomodations = accomodations
+const AboutElements = aboutElements
+
 
 function getAllAccomodations() {
     return Accomodations
@@ -7,8 +10,13 @@ function getAllAccomodations() {
 function getAccomodation(id) {
     return Accomodations.find(elt=>id ===elt.id)
 }
+function getAllAboutElements() {
+    return AboutElements
+    
+}
 
 export {
     getAccomodation,
+    getAllAboutElements,
     getAllAccomodations
 }
