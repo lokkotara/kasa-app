219760d12b36dd60ba52b1260067260461c9362e
greg <lokkotarakun@gmail.com>
2022-06-07 23:51:50 +0200

    refactor: Séparation de la logique de routage dans un composant dédié

---
+++ src/Router.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Accomodation from './pages/Accomodation/Accomodation';
+import Error from './pages/Error/Error';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+
+export default function router() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/accomodation/:id" element={<Accomodation />} />
+        <Route path="/error" element={<Error />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,26 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style/index.scss';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from './pages/Home/Home';
-import About from './pages/About/About';
-import Accomodation from './pages/Accomodation/Accomodation';
-import Error from './pages/Error/Error';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
+import Router from './Router';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <Router>
-    <Header />
-    <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="About" element={<About />} />
-      <Route path="Accomodation/:id" element={<Accomodation />} />
-      <Route path="*" element={<Error />} />
-    </Routes>
-    <Footer />
-  </Router>,
+  <Router />,
   document.getElementById('root')
 );
 
